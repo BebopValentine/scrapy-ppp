6f2d42354212b6a8edcb28e5ada2c041dbc93f97
ShroXd <shroxdf@gmail.com>
2020-01-13 19:38:23 +0800

    增加了爬虫开始，结束的邮件通知

--- setu/pipelines.py
+++ setu/pipelines.py
@@ -8,11 +8,21 @@
 import pymongo
 from setu.items import BookInfo, BookChapters, BookContents, IpPool, ChengduTenancy
 
+from email.mime.text import MIMEText
+from email.header import Header
+from smtplib import SMTP_SSL
+
 
 class SetuPipeline(object):
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
+
+        self.host_server = 'smtp.163.com'
+        self.sender_qq = 'shroudfzj@163.com'
+        self.pwd = 'fzjinspur0203'
+        self.sender_qq_mail = 'shroudfzj@163.com'
+        self.receiver = '524061832@qq.com'
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -22,6 +32,8 @@ class SetuPipeline(object):
         )
 
     def open_spider(self, spider):
+        self.send_mail('爬虫开始工作')
+
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
 
@@ -87,4 +99,25 @@ class SetuPipeline(object):
             return item
 
     def close_spider(self, spider):
+        self.send_mail('爬虫结束工作')
+
         self.client.close()
+
+    def send_mail(self, title):
+        # 邮件的正文内容
+        mail_content = '你好，爬虫结束工作'
+        # 邮件标题
+        mail_title = title
+
+        smtp = SMTP_SSL(self.host_server)
+        # set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+        smtp.set_debuglevel(1)
+        smtp.ehlo(self.host_server)
+        smtp.login(self.sender_qq, self.pwd)
+
+        msg = MIMEText(mail_content, "plain", 'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
+        msg["From"] = self.sender_qq_mail
+        msg["To"] = self.receiver
+        smtp.sendmail(self.sender_qq_mail, self.receiver, msg.as_string())
+        smtp.quit()
